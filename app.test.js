a60826be4c60e1ba4f92b0f156d956623c08ed2d
raylin0111 <railinmena111@gmail.com>
2023-12-15 19:53:40 -0400

    ola

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@
 const saludar = require('./app');
 
 
-module.exports = saludar
+
 test("de esperar un hola mundo", ()=>{
-    expect(saludo()).toBe("hola mundo")
+    expect(saludar()).toBe("hola mundo")
 })
